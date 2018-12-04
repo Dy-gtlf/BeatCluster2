fec703dcaf7e5cf0ff7c0e48741dc36051e78ffb
Dy-gtlf <gootalife0512@gmail.com>
2018-12-05 00:31:17 +0900

    ステージと背景

---
+++ Assets/Scripts/Play/Note.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+	public int lineNum;
+	public float judgeTime; 
+
+	public Note (int num) {
+		lineNum = num;
+	}
+}
