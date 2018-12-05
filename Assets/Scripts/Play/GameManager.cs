6114df307c0014237caf9a710c33669c898426f1
daidai-gtlf <gootalife0512@gmail.com>
2018-12-05 20:10:13 +0900

    スクリプト追加

--- Assets/Scripts/Play/GameManager.cs
+++ Assets/Scripts/Play/GameManager.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-
-	// Use this for initialization
+	private Score score = new Score(); 
+	private List<List<GameObject>> notes = new List<List<GameObject>>();
+	private float speed;
+	private float time;
 	void Start () {
-		
+		// 譜面の読み込み
+
+		// 曲の読み込み
+
+		// ノーツ生成
+
+		// 曲の開始
 	}
 	
-	// Update is called once per frame
 	void Update () {
+		// 可視状態ならノーツを移動
 		
 	}
 }
---
+++ Assets/Scripts/Play/Score.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Score {
+    public string name;
+    public string description;
+    public int def_bpm;
+    public int def_beat;
+}
